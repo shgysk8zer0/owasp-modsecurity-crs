4db413cd3268b93ea53d2c61d49895114325e1e2
Chris Zuber <shgysk8zer0@gmail.com>
2015-03-18 08:06:22 -0700

    Do checks before enabling or disabling rules
    Do not enable if already enabled, and do not
    disable if already disabled

--- enable_slr_rules.bash
+++ enable_slr_rules.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 rules=($(ls slr_rules/))
-for f in ${rules[@]}
-	do ln -s $(pwd)/base_rules/$f activated_rules/$f
+for f in ${rules[@]}; do
+	if [ ! -f slr_rules/$f ]; then
+		ln -s $(pwd)/slr_rules/$f activated_rules/$f
+	fi
 done
